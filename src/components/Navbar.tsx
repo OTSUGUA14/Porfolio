a559d6dd861179c3eec0c1157233bbed68147b27
Augusto <otsuguariquelme@gmail.com>
2025-01-30 13:51:56 -0300

    augusto

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav className=" w-full  p-4 flex justify-between items-center  bg-slate-200">
       <div className="text-[#0d1321] font-bold flex flex-row items-center ml-4">
-        <img className="size-20 rounded-full shadow-md shadow-black" src="public/imagenes/augusto.jpg" alt="imgen mia" />
+        <img className="size-20 rounded-full shadow-md shadow-black" src="imagenes/augusto.jpg" alt="imgen mia" />
         <h6 className="pl-5 text-3xl">Bienvenidos</h6>
       </div>
 
